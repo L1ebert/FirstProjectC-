10f8146c5faeabb7c42edfdd7a935d201495613f
Николай Толмачев <niktolm556@gmail.com>
2024-04-06 19:49:39 +0300

    Добавьте файлы проекта.

---
+++ FirstProjectC++/Source.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main()
+{
+	/* 
+	printf("va");
+	cout << "Hello World!";
+	cout << "\n" << 
+	endl;
+	string str;
+	str = "123";
+	cin >> str;
+	cout << str;
+	*/
+
+	/*
+	 int i1 = -26346785775;
+	bitset<32> b1(i1 -1);
+	cout << i1 << '\n' << b1 << '\n';
+	int i2 = -9885722;
+	bitset<32> b2(i1);
+	cout << i2 << '\n' << b2;
+	*/
+	double d = 13.12;
+	cout << d;
+
+	char c = 'd';
+	cout << c;
+
+	int i = 123;
+
+	c = d;
+	cout << c << '\n';
+
+	cout << (int)c;
+	/*
+	БАЗА, пока учу базовые функции на С++,
+	есть флэшбэки с С#.
+	*/
+}
